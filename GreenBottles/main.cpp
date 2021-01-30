f237f72a07fcec7e4df313269350165367b23515
nathan <spuddie1984@hotmail.com>
2021-01-30 21:37:28 +0800

    Getting to Know the Ide Assignment

---
+++ GreenBottles/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int greenBottles = 10;
+
+    cout << "There were " << greenBottles << " green bottles" << endl;
+
+    greenBottles--;
+
+    cout << "There were " << greenBottles << " green bottles" << endl;
+    return 0;
+}
